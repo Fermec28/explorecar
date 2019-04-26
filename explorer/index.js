286faea9a8fdcd22cddbdcd9df09d72dc84cbeb2
Jose Medina <fermed28@gmail.com>
2019-04-26 10:54:32 -0500

    [feature] works on local network

--- explorer/index.js
+++ explorer/index.js
@@ -5,18 +5,18 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const wCap = new cv.VideoCapture(0);
-
+const FPS=30;
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 setInterval(()=> {
-    const frame = wCap.read();
-    const image = cv.imencode('.jpg', frame).toString('base64');
-    io.emit('image', image);
-}, 30);
+  	const frame = wCap.read();
+	const image = cv.imencode('.jpg', frame).toString('base64');
+  	io.emit('image', image);
+}, 1000 / FPS );
 
-server.listen(3000, ()=>{
+server.listen(3000,'0.0.0.0', ()=>{
     console.log('App listening on port 3000!');
 });
