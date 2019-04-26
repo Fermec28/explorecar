4ed832682b384eef61e4ed3c0ae962b97ae6d70f
Jose Medina <fermed28@gmail.com>
2019-04-26 13:45:48 -0500

    [added] simple procedural model structure

--- navigator/car.py
+++ navigator/car.py
@@ -1,25 +1,73 @@
+#import RPi.GPIO as GPIO
+# lets us have a delay
+from time import sleep
+
+wheel1_0 = 24
+wheel1_1 = 24
+wheel2_0 = 24
+wheel2_1 = 24
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(24, wheel1_0) #0 wheel 1
+#GPIO.setup(24, wheel1_1) #1 wheel 1
+#GPIO.setup(24, wheel2_0) #0 wheel 2
+#GPIO.setup(24, wheel2_1) #1 wheel 2
 #Model of simple car
 class Car:
     @classmethod
     def foward(cls):
+        #set the ports correctly
+        #GPIO.output(wheel1_0, 1)
+        #GPIO.output(wheel1_1, 0)
+        #GPIO.output(wheel2_0, 1)
+        #GPIO.output(wheel2_1, 0)
+
+        sleep(.30)
         print("move the car foward")
+        cls.stop()
 
     @classmethod
     def rigth(cls):
-         print("move the car on the rigth")
+        #set the ports correctly
+        #GPIO.output(wheel1_0, 0)
+        #GPIO.output(wheel1_1, 1)
+        #GPIO.output(wheel2_0, 1)
+        #GPIO.output(wheel2_1, 0)
+
+        sleep(.30)
+        print("move the car on the rigth")
+        cls.stop()
 
     @classmethod
     def left(cls):
-         print("move the car on the left")
+        #set the ports correctly
+        #GPIO.output(wheel1_0, 1)
+        #GPIO.output(wheel1_1, 0)
+        #GPIO.output(wheel2_0, 0)
+        #GPIO.output(wheel2_1, 1)
+
+        sleep(.30)
+        print("move the car on the left")
+        cls.stop()
 
     @classmethod
     def reverse(cls):
-         print("move the car on the reverse")
+        #set the ports correctly
+        #GPIO.output(wheel1_0, 0)
+        #GPIO.output(wheel1_1, 1)
+        #GPIO.output(wheel2_0, 0)
+        #GPIO.output(wheel2_1, 1)
+
+        sleep(.30)
+        print("move the car on the reverse")
+        cls.stop()
 
     @classmethod
     def stop(cls):
         print("stop the car")
-
+        #GPIO.output(wheel1_0, 0)
+        #GPIO.output(wheel1_1, 0)
+        #GPIO.output(wheel2_0, 0)
+        #GPIO.output(wheel2_1, 0)
 
 
 
