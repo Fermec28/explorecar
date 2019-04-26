4672ef32498a01075f4eb06212590249140bf160
Jose Medina <fermed28@gmail.com>
2019-04-25 20:46:23 -0500

    render image from camera0

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const wCap = new cv.VideoCapture(0);
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 setInterval(()=> {
-    io.emit('image','some data');
-}, 1000);
+    const frame = wCap.read();
+    const image = cv.imencode('.jpg', frame).toString('base64');
+    io.emit('image', image);
+}, 30);
 
 server.listen(3000, ()=>{
     console.log('App listening on port 3000!');
