40ccc0f0bc1a53bb62facd66c6a97c228587b864
Jose Medina <fermed28@gmail.com>
2019-04-28 22:33:25 -0500

    [feature] comunication both servers Fixes #2 Fixes #3

--- explorer/index.js
+++ explorer/index.js
@@ -4,8 +4,14 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const axios = require('axios');
 const wCap = new cv.VideoCapture(0);
 const FPS=30;
+
+
+async function getJSONAsync(action){
+    return await axios.get('http://localhost:5000/'+ action);
+}
 
 app.use(express.static(path.join(__dirname, 'static')));
 
@@ -13,11 +19,38 @@ app.get('/', (req, res) => {
     res.sendFile('index.html');
 })
 
+io.on('connection',function(socket){
+
+    socket.on('foward', function(){
+	getJSONAsync('foward').then( function(result) {
+	    console.log('moving to foward');
+	});
+    });
+
+    socket.on('rigth', function(){
+	getJSONAsync('rigth').then( function(result) {
+	    console.log('moving to rigth');
+	});
+    });
+
+    socket.on('left', function(){
+	getJSONAsync('left').then( function(result) {
+	    console.log('moving to left');
+	});
+    });
+
+    socket.on('reverse', function(){
+	getJSONAsync('left').then( function(result) {
+	    console.log('moving to reverse');
+	});
+    });
+});
+
 setInterval(()=> {
   	const frame = wCap.read();
 	const image = cv.imencode('.jpg', frame).toString('base64');
   	io.emit('image', image);
-}, 1000 / FPS );
+    }, 1000 / FPS );
 
 server.listen(3000,'0.0.0.0', ()=>{
     console.log('App listening on port 3000!');
--- explorer/static/js/app.js
+++ explorer/static/js/app.js
@@ -1,54 +1,26 @@
 console.log('Hola Mundo')
-/*
-  Plantar el evento del teclado
-  leer sobre promises
-  usar wait sobre promises para enviar la peticion ajax
-  url/car/foward
-  url/car/right
-  url/car/left
-  url/car/reverse
-  Seria genial usar JWT para que las solicitudes sean aceptadas
-  solo por este servidor
-*/
-const URI = 'http://localhost:5000'
-
-ajaxCall = function(action){
-    $.ajax({
-        url: URI+'/'+ action,
-        type: 'GET',
-        async: false,
-        cache: false,
-        timeout: 300,
-        error: function(){
-            return true;
-        },
-        success: function(msg){
-            console.log(msg)
-        }
-    });
-}
 
 $(document).ready(function(){
     $(document).keydown(function(e) {
     switch(e.which) {
         case 37: // left
 	console.log('left');
-	ajaxCall('left');
+	socket.emit('left')
 	break;
 
         case 38: // up
 	console.log('foward');
-	ajaxCall('foward');
+	socket.emit('foward');
 	break;
 
         case 39: // right
 	console.log('rigth');
-	ajaxCall('rigth');
+	socket.emit('rigth');
 	break;
 
         case 40: // down
 	console.log('reverse');
-	ajaxCall('reverse');
+	socket.emit('reverse');
 	break;
 
         default: return; // exit this handler for other keys
